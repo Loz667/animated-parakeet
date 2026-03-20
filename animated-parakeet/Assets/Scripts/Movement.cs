3ae0c5c46e5b5394cea131cf358e1f8f4ed298c9
Loren Penny <62217245+Loz667@users.noreply.github.com>
2026-03-20 12:30:17 +0000

    Added jump function

---
+++ animated-parakeet/Assets/Scripts/Movement.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Movement : MonoBehaviour
+{
+    [SerializeField] InputAction propel;
+
+    [SerializeField] float propelForce = 100f;
+
+    Rigidbody rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void OnEnable()
+    {
+        propel.Enable();
+    }
+
+    private void FixedUpdate()
+    {
+        if (propel.IsPressed())
+        {
+            //rb.AddRelativeForce(Vector3.up * propelForce * Time.fixedDeltaTime);
+
+            Vector2 playerVelocity = rb.linearVelocity;
+            playerVelocity.y = 0f;
+            rb.linearVelocity = playerVelocity;
+
+            rb.AddForce(Vector2.up * propelForce, ForceMode.Impulse);
+        }
+    }
+}
